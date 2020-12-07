a9aef7f8c63deb1216de3274db84f222953561c3
sachdavesingh <31259086+sachdavesingh@users.noreply.github.com>
2020-12-06 23:47:40 -0800

    Add files via upload
    Final built of app

---
+++ duplicate 2/Reminder/EntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryViewController.swift
+//  Reminder
+//
+//  Created by Sachdave Singh on 12/6/20.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+
+        public var completion: ((String, String) -> Void)?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            titleField.becomeFirstResponder()
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+        }
+
+        @objc func didTapSave() {
+            if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
+                completion?(text, noteField.text)
+    
+            }
+        }
+
+}
